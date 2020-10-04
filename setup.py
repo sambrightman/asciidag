150fcf1a4d21c8cb34abdf447da44e8b9e5c54ae
Sam Brightman <sam.brightman@gmail.com>
2020-10-04 16:11:51 +0100

    Add dev requirements to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,32 @@ def main():
         image_base_url = urljoin("https://raw.githubusercontent.com/", relative_package_path)
         readme = rewrite_relative_images(image_base_url, readme_file.read())
 
+    tests_require = [
+        "astroid<2; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "flake8; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
+        "flake8<3.8; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "flake8_docstrings",
+        "isort<5",
+        "more-itertools<6",
+        "py>=1.4.29",
+        "pydocstyle<4",
+        "pylint-venv<2",
+        "pylint<2; python_version<'3.5'",
+        "pylint>=2; python_version>='3.5'",
+        "pyparsing<3",
+        "pytest-cov; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
+        "pytest-cov<2.6; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "pytest-flake8; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
+        "pytest-flake8<0.9; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "pytest-pylint<0.10; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "pytest-pylint<0.15; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
+        "pytest<3; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
+        "pytest>=2.7,<4.7; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
+        "typed_ast<1.3; python_version>='3' and python_version<'3.5'",
+        "typing<3.7; python_version>='3' and python_version<'3.5'",
+        "zipp<3",
+    ]
+
     setup(
         name=metadata["title"],
         version=metadata["version"],
@@ -52,31 +78,13 @@ def main():
         install_requires=[
             "enum34; python_version<'3.4'",
         ],
-        tests_require=[
-            "astroid<2; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "flake8; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
-            "flake8<3.8; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "flake8_docstrings",
-            "isort<5",
-            "more-itertools<6",
-            "py>=1.4.29",
-            "pydocstyle<4",
-            "pylint-venv<2",
-            "pylint<2; python_version<'3.5'",
-            "pylint>=2; python_version>='3.5'",
-            "pyparsing<3",
-            "pytest-cov; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
-            "pytest-cov<2.6; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "pytest-flake8; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
-            "pytest-flake8<0.9; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "pytest-pylint<0.10; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "pytest-pylint<0.15; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
-            "pytest<3; python_version<'2.7' or (python_version>='3' and python_version<'3.4')",
-            "pytest>=2.7,<4.7; (python_version>='2.7' and python_version<'3') or python_version>='3.4'",
-            "typed_ast<1.3; python_version>='3' and python_version<'3.5'",
-            "typing<3.7; python_version>='3' and python_version<'3.5'",
-            "zipp<3",
-        ],
+        tests_require=tests_require,
+        extras_require={
+            "dev": tests_require + [
+                "bump2version",
+                "twine",
+            ],
+        },
         license=metadata["license"],
         classifiers=[
             "Development Status :: 3 - Alpha",
